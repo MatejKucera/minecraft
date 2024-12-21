93b356e473c8a606f523f1edb027a8926f63ebd8
Matěj Kučera <matej@tailstrike.cz>
2024-12-21 20:21:09 +0100

    upgrade to 1.21.1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-#if [ -f /app/.env ]
-#then
-#  export $(cat /app/.env | sed 's/#.*//g' | xargs)
-#fi
-
-#echo "helo!"
-#echo "oops"
-
 echo "eula=true" > eula.txt
 
 if [ $RUN_SERVER = "true" ] ; then
     echo "is true";
-    java -Xms4G -Xmx4G -XX:+UseG1GC -jar /app/spigot-1.19.3.jar nogui
+    java -Xms4G -Xmx4G -jar /app/spigot-1.21.1.jar nogui
 else
     echo "is false";
     tail -f /dev/null
