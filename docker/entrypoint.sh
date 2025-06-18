d811e9895645a6e114f23b7fa8d9ccab509685ab
Matěj Kučera <matej@tailstrike.cz>
2025-06-18 16:31:33 +0200

    update to 1.6

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ echo "eula=true" > eula.txt
 
 if [ $RUN_SERVER = "true" ] ; then
     echo "is true";
-    java -Xms4G -Xmx4G -jar /app/spigot-1.21.1.jar nogui
+    java -Xms4G -Xmx4G -jar /app/spigot-1.21.6.jar nogui
 else
     echo "is false";
     tail -f /dev/null
