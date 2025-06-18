1b7524f161dcd26d80d5a196312a6cfd43954ff3
Matěj Kučera <matej@tailstrike.cz>
2025-06-18 16:46:56 +0200

    more memory and ignore worlds

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ echo "eula=true" > eula.txt
 
 if [ $RUN_SERVER = "true" ] ; then
     echo "is true";
-    java -Xms4G -Xmx4G -jar /app/spigot-1.21.6.jar nogui
+    java -Xms8G -Xmx8G -jar /app/spigot-1.21.6.jar nogui
 else
     echo "is false";
     tail -f /dev/null
