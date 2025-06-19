ad56ce1fdecb75c60eebcc19c47523997dfe7b3e
Matěj Kučera <matej@tailstrike.cz>
2025-06-19 13:17:33 +0200

    fixed backup

--- backup.sh
+++ backup.sh
@@ -3,43 +3,43 @@
 # cd to main directory
 cd /root/apps/minecraft
 
-# Compress the contents of directory ./worlds with maximum compression
-tar -cvzf worlds.tar.gz --use-compress-program=pigz ./worlds
-
-# Check if the tar command was successful
+# Compress the contents of directory ./worlds using zip
+FILENAME="worlds"
+zip -r $FILENAME.zip ./worlds
+# Check if the zip command was successful
 if [ $? -eq 0 ]; then
-    echo "Backup created successfully: worlds.tar.gz"
+    echo "Compression successful: $FILENAME.zip"
 else
-    echo "Error creating backup."
+    echo "Error compressing worlds directory."
     exit 1
 fi
 
 # Move the backup to directory ./backups with current timestamp as filename
 timestamp=$(date +"%Y%m%d_%H%M%S")
-mv worlds.tar.gz ./backups/worlds_backup_$timestamp.tar.gz
+mv worlds.zip ./backups/worlds_backup_$timestamp.zip
 
 # Check if the move command was successful
 if [ $? -eq 0 ]; then
-    echo "Backup moved successfully to ./backups/worlds_backup_$timestamp.tar.gz"
+    echo "Backup moved successfully to ./backups/worlds_backup_$timestamp.zip"
 else
     echo "Error moving backup."
     exit 1
 fi
 
 # Remove the original tar.gz file if it exists
-if [ -f ./worlds.tar.gz ]; then
-    rm ./worlds.tar.gz
-    echo "Removed original worlds.tar.gz file."
+if [ -f ./worlds.zip ]; then
+    rm ./worlds.zip
+    echo "Removed original worlds.zip file."
 else
-    echo "Original worlds.tar.gz file does not exist."
+    echo "Original worlds.zip file does not exist."
 fi
 
 # Upload the backup to remove server using FTP
-sshpass -p 'r_Xt94fC' scp -P 20135 $FILENAME.gz eslib_backup@192.168.0.4:/backup/minecraft.gz
+sshpass -p 'r_Xt94fC' scp -P 20135 $FILENAME.zip eslib_backup@192.168.0.4:/backup/minecraft.zip
 
 
 # Remove all backups that are older than 7 days
-find ./backups -type f -name "worlds_backup_*.tar.gz" -mtime +7 -exec rm {} \;
+find ./backups -type f -name "worlds_backup_*.zip" -mtime +7 -exec rm {} \;
 
 # Check if the find command was successful
 if [ $? -eq 0 ]; then
